1efe45f70b2345bde3eafbfe16c3d0865d66110e
itszerofour <itszerofourx@gmail.com>
2024-02-29 18:09:11 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,42 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+dotenv.config({ path: "./.env" });
+const app = express();
+
+/* CONSTANTS */
+const PORT = process.env.PORT || 5000;
+/* MIDDLEWARES */
+app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "200mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "200mb",
+    extended: true,
+    parameterLimit: 1000000,
+  })
+);
+
+/* ROUTES */
+
+/* START FUNCTION */
+async function start() {
+  try {
+    app.listen(PORT, (err) => {
+      if (err) return console.log("App crashed: ", err);
+      console.log(`Server started successfully! Port: ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+start();
